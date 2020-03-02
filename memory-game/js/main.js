b0df66b93549ea72faa188b69250d3e31f4b42a4
carlynicholson <carlypnicholson@gmail.com>
2020-03-02 11:17:32 -0600

    commit

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,41 +1,56 @@
-const cards = [{
-	rank: 'queen', suit: 'hearts', cardImage: 'images/queen-of-hearts.png' },
-{
-	rank: 'queen',
-	suit: 'diamonds',
-	cardImage: 'images/queen-of-diamonds.png'
-}, 
-{rank: 'king', suit: 'hearts', cardImage: 'images/king-of-hearts.png'},
-{rank: 'king', suit: 'diamonds', cardImage: 'images/king-of-diamonds.png'}
-]
-let cardsInPlay= []
-function flipCard() {
-	const cardId = this.getAttribute('data-id')
-//console.log("Up and running!");
-console.log("User flipped" + cards[cardId].rank) 
-console.log(cards[cardId].cardImage)
-console.log(cards[cardId].suit)
-cardsInPlay.push(cards[cardId])
-}
-
-function checkForMatch () {
-//step 11, look at this one later
-	this.setAttribute('src', cards[cardId].cardImage)
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-  		console.log("You found a match!");
+const cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "https://res.cloudinary.com/df6sigxz7/image/upload/v1583166627/queen-of-hearts_y733gt.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "https://res.cloudinary.com/df6sigxz7/image/upload/v1583166627/queen-of-diamonds_xl1brj.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "https://res.cloudinary.com/df6sigxz7/image/upload/v1583166627/king-of-hearts_oln0nc.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "https://res.cloudinary.com/df6sigxz7/image/upload/v1583166627/king-of-diamonds_bh3mty.png"
+	},
+	];
+	const cardsInPlay = [];
+	
+	function checkForMatch() {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+	  alert("You found a match!");
 	} else {
-  		console.log("Sorry, try again.");
+	  alert("Sorry, try again.");
 	}
-}
-
-function createBoard() {
-    for (let i = 0; i < cards.length; i++) {
-        let cardElement = document.createElement("img")
-		cardElement.setAttribute("src", cards[i].cardImage)
-		cardElement.setAttribute("data-id", i)
-		cardElement.addEventListener('click', flipCard)
-		document.getElementById('game-board').appendChild(cardElement)
-    }
-}
-
-createBoard()
+	}
+	
+	
+	function flipCard() {
+		let cardId = this.getAttribute('data-id');
+		console.log("User flipped " + cards[cardId].rank);
+		cardsInPlay.push(cards[cardId].rank);
+		console.log(cards[cardId].cardImage);
+		console.log(cards[cardId].suit);
+		this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+	}
+	
+	function createBoard(){
+		for (let i = 0; i < cards.length; i++) {
+			let cardElement = document.createElement('img');
+			cardElement.setAttribute('src', 'images/back.png');
+			cardElement.setAttribute('data-id' ,i);
+			document.getElementById('game-board').appendChild(cardElement);
+			cardElement.addEventListener('click', flipCard);
+		}
+	}
+	
+	createBoard();
